b1b55d0c84aca45a066c26b2dded54822526b431
plangones <plangmatheus39@gmail.com>
2024-10-08 19:10:45 -0300

    Add files via upload

---
+++ ComboBoxTest.java
@@ -0,0 +1,16 @@
+// Fig. 12.22: ComboBoxTest.java
+// Testing ComboBoxFrame.
+import javax.swing.*;// importacao 
+
+public class ComboBoxTest
+{
+   public static void main(String[] args)
+   { // abertura do bloco de codigos 
+      ComboBoxFrame comboBoxFrame = new ComboBoxFrame(); // declaração de variavel com atribuicao de um novo objeto
+      comboBoxFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);// definiçao da operacao do fechamento padrao
+      comboBoxFrame.setSize(350, 150); // configuracao de tamanho 
+      comboBoxFrame.setVisible(true); // deixa visivel na tela 
+   } // encerramento do bloco de codigos 
+} // final da  classe ComboBoxTest
+
+
